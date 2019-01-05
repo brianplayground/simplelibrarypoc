87353792cead61befcc0ea28cf5cd879a4764b1a
BrianPhiri <brianphiri.9523@gmail.com>
2019-01-06 00:48:18 +0300

    added uis

---
+++ app/src/main/java/com/brianphiri/simplelibrarypoc/ui/NewBookActivity.kt
@@ -0,0 +1,13 @@
+package com.brianphiri.simplelibrarypoc.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brianphiri.simplelibrarypoc.R
+
+class NewBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_book)
+    }
+}
